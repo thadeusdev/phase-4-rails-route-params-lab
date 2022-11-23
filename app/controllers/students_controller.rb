610a8fafc222f61e1f591178891aef827b24b022
thadeus <thadeusnyariki94@gmail.com>
2022-11-23 10:38:28 +0300

    Done assignment

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -5,4 +5,15 @@ class StudentsController < ApplicationController
     render json: students
   end
 
+  def show
+    student = Student.find(params[:id])
+    render json: student
+  end
+
+  # def self.search(keywords)
+  #   if keywords
+  #     where(:all, :conditions => ["concat(first_name," ",last_name) like?", "%#{keywords}%"])
+  #   end
+  # end
+
 end
